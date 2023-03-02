0d3a3c7cb8317e22aa933c564ce94d488677edbf
Sultani Sanan <54854030+sultani93@users.noreply.github.com>
2023-03-02 18:20:26 +0400

    cart image downloaded from api

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,8 +1,13 @@
+import Card from './Card'
+
 export default function Results({ results }) {
   return (
-    <div>
+    <div
+      className='sm:grid sm:grid-cols-2 lg:grid lg:grid-cols-3 xl:grid xl:grid-cols-4 2xl:grid 2xl:grid-cols-5
+    max-2-6xl mx-auto py-4'
+    >
       {results.map((result) => (
-        <div key={result.id}>{result.original_title}</div>
+        <Card key={result.id} result={result} />
       ))}
     </div>
   )
