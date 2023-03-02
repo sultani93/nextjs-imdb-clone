97bbc9a6bf88d723545a2cb470019955ca6bb213
Sultani Sanan <54854030+sultani93@users.noreply.github.com>
2023-03-02 18:00:21 +0400

    loading spinner

---
+++ src/components/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='flex justify-center'>
+      <img className='h-96' src='spinner.svg' alt='loading-spinner' />
+    </div>
+  )
+}
+
+export default loading
