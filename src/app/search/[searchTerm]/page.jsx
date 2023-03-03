d219d05789c22fa00d0f6c25c0b8137a1fccf7ff
Sultani Sanan <54854030+sultani93@users.noreply.github.com>
2023-03-03 15:38:08 +0400

    seaech ability

---
+++ src/app/search/[searchTerm]/page.jsx
@@ -0,0 +1,25 @@
+import Results from '@/components/Results'
+import React from 'react'
+
+const SearchPage = async ({ params }) => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${params.searchTerm}`
+  )
+
+  if (!res.ok) {
+    throw new Error('error fetching data')
+  }
+  const data = await res.json()
+  const results = data.results
+
+  return (
+    <div>
+      {results && results.length === 0 && (
+        <h1 className='text-center pt-6'>No results found</h1>
+      )}
+      {results && <Results results={results} />}
+    </div>
+  )
+}
+
+export default SearchPage
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,7 +4,7 @@ export default function Results({ results }) {
   return (
     <div
       className='sm:grid sm:grid-cols-2 lg:grid lg:grid-cols-3 xl:grid xl:grid-cols-4 2xl:grid 2xl:grid-cols-5
-    max-2-6xl mx-auto py-4'
+    max-w-6xl mx-auto py-4'
     >
       {results.map((result) => (
         <Card key={result.id} result={result} />
